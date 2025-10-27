0dac169a4035f9c50381a0a3005a8e143bf9976d
a$ong <fwscool@163.com>
2025-10-28 02:04:47 +0800

    尝试灯光效果

--- UI/title.gd
+++ UI/title.gd
@@ -14,3 +14,8 @@ func _process(delta: float) -> void:
 func _on_quit_pressed() -> void:
 	get_tree().quit()
 	pass # Replace with function body.
+
+
+func _on_single_pressed() -> void:
+	get_tree().change_scene_to_file("res://Levels/38f.tscn")
+	pass # Replace with function body.
