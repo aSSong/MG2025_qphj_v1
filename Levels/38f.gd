1dc74e8cb89d0235e1312aa497d66abd93fb9df8
a$ong <fwscool@163.com>
2025-10-29 10:08:28 +0800

    创建所有players以及相机

---
+++ Levels/38f.gd
@@ -0,0 +1,14 @@
+extends Node2D
+class_name Arena
+
+@export var player: Player
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Global.player = player
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
